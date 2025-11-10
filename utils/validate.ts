d1ca1eb1d7344174f958f01d54647b554f7e58e6
unknown <tjfgml8054@naver.com>
2025-11-11 00:27:12 +0900

    [week6/mission]useQuery로 LP 목록/상세 화면 만들기

---
+++ utils/validate.ts
@@ -0,0 +1,30 @@
+export type UserSigninInformation = {
+    email: string;
+    password: string;
+};
+
+export function validateUser(values: UserSigninInformation) {
+    const errors = {
+        email: "",
+        password: "",
+    };
+
+
+    // 이메일 형식에 대한 검사
+    if (!/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i.test(
+        values.email,
+    )){
+        errors.email = "올바른 이메일 형식이 아닙니다";
+    }
+
+    if (!(values.password.length >= 8 && values.password.length < 20)){
+        errors.password = "비밀번호는 8~20자 사이로 입력해주세요";
+    }
+
+    return errors;
+}
+
+// 로그인 유효성 검사 함수
+export function validateSignin(values: UserSigninInformation){
+    return validateUser(values);
+}
