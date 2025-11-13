25ebe659bf25d84aee8641cbd0fa2d4aa3f2d008
unknown <tjfgml8054@naver.com>
2025-11-13 19:11:39 +0900

    [week7/mission1]useMutation을 활용하여 서버 상태 손쉽게 관리하기

---
+++ src/constants/key.ts
@@ -0,0 +1,13 @@
+export const LOCAL_STORAGE_KEY = {
+  accessToken: "accessToken",
+  refreshToken: "refreshToken",
+};
+
+// ✅ React Query에서 사용하는 쿼리 키
+export const QUERY_KEY = {
+  lps: "lps", // LP 목록
+  lp: "lp",   // LP 상세
+  user: "user", // 사용자 정보
+  likes: "likes", // 좋아요 토글 관련
+    myInfo: "myInfo",
+};
